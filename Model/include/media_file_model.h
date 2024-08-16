d4dcbef6926ac12823606c1c8c238d38d3547612
VuLHL <lamvuoxv@gmail.com>
2024-08-16 09:12:08 +0700

    add

---
+++ Model/include/media_file_model.h
@@ -0,0 +1,16 @@
+#ifndef MEDIA_FILE_MODEL_H
+#define MEDIA_FILE_MODEL_H
+
+#include <string>
+#include <iostream>
+
+class MediaFileModel{
+private:
+    
+public:
+    MediaFileModel(/* args */);
+    MediaFileModel();
+};
+
+
+#endif
